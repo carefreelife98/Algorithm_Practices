680a2e53f358d509784f478848fe02b36115efbd
SeungminChae <csm4903@naver.com>
2023-06-21 18:16:39 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+n = int(input())
+grade = n // 10
+if grade >= 9:
+    print('A')
+elif grade == 8:
+    print('B')
+elif grade == 7:
+    print('C')
+elif grade == 6:
+    print('D')
+else:
+    print('F')
