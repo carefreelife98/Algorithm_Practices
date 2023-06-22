b770902b5c909b7fd506a7f8c65346401e3a2273
SeungminChae <csm4903@naver.com>
2023-06-22 22:50:46 +0900

    [Bronze V] Title: A+B - 7, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.py
@@ -0,0 +1,10 @@
+import sys
+
+t = int(input())
+rs = []
+for i in range(t):
+    a, b = sys.stdin.readline().split()
+    idx = 0
+    rs.append(int(a) + int(b))
+for i in range(len(rs)):
+    print(f'Case #{i + 1}: {rs[i]}')
